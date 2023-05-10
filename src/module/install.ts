f4d4945343a7d63b795f45ddbe4f981ee8d24633
Code pianist <77950104+1739616529@users.noreply.github.com>
2023-05-11 00:53:01 +0800

    feat: fish install fix bug

--- src/module/install.ts
+++ src/module/install.ts
@@ -9,28 +9,28 @@ import { rm, cp } from "shelljs";
 const test_reg = /NSV_HOME|nsv discern/
 export function install () {
     let content = ""
-    
+
     ensureFileSync(shellConfigFileDir)
     const shellConfigFileStr = readFileSync(shellConfigFileDir, {encoding: "utf-8"})
-    
+
     if (test_reg.test(shellConfigFileStr)) return console.log("nsv: installed")
 
     const shell_config_file_content = shellConfigFileStr.split(EOL)
-    
+
     const { home } = context.get("dir")
     // linux macos
     if (system === "linux" || system === "darwin") {
 
         // fish
         if (shell === "fish") {
-            copyFile(join(home, "nsv.fish"), shellConfigFileDir)
+            writeFile(shellConfigFileDir, readFileSync(join(home, "nsv.fish")))
         } else {
             // bash zsh
             shell_config_file_content.push(`export NSV_HOME=$HOME/.nsv`)
             shell_config_file_content.push(`[ -s "$NSV_HOME/nsv.sh" ] && . "$NSV_HOME/nsv.sh"`)
             shell_config_file_content.push(`export PATH=$NSV_HOME/local/node/bin:$PATH`)
         }
-        
+
     } else // win
     if (system === "win") {
         shell_config_file_content.push(`$Env:NSV_HOME = "${home}"`)
@@ -43,15 +43,18 @@ export function install () {
 }
 
 export function uninstall () {
-    ensureFileSync(shellConfigFileDir)
-    let shell_config_file_content = readFileSync(shellConfigFileDir, { encoding: "utf-8" }).toString().split(EOL).filter(v => !test_reg.test(v))
-    writeFile(shellConfigFileDir, shell_config_file_content.join(EOL), { encoding: "utf-8" })
 
-    
+
     /**
      * 如果用的是fish  需要删除 fish functions 中的环境变量
      */
     if(shell === "fish") {
-        rm(join(dirname(shellConfigFileDir), "functions", "nsv.fish"))
+        rm(shellConfigFileDir)
+    } else
+    {
+        ensureFileSync(shellConfigFileDir)
+        let shell_config_file_content = readFileSync(shellConfigFileDir, { encoding: "utf-8" }).toString().split(EOL).filter(v => !test_reg.test(v))
+        writeFile(shellConfigFileDir, shell_config_file_content.join(EOL), { encoding: "utf-8" })
+
     }
 }
