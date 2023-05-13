d40b451ccf6b68ad9f816b5f5fd6fddaf49820a7
Code pianist <77950104+1739616529@users.noreply.github.com>
2023-05-13 20:52:28 +0800

    feat: prefix

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # nsv
 一个跨平台node版本管理工具
+https://img.shields.io/github/languages/code-size/1739616529/nsv
+![Markdown 图标](https://markdown-here.com/img/icon256.png)
--- src/module/install.ts
+++ src/module/install.ts
@@ -2,9 +2,10 @@
 import { join, dirname } from "path";
 import { EOL } from "os";
 import { system, shell, shellConfigFileDir, prefix } from "../../local.json"
-import { copyFile, ensureFileSync, readFileSync, writeFile, constants } from "fs-extra";
+import { copyFile, ensureFileSync, readFileSync, writeFile, constants, existsSync } from "fs-extra";
 import { context } from "../context"
 import { rm, cp } from "shelljs";
+import { prefix as config_prefix } from "../../config.json"
 
 const test_reg = /NSV_HOME|nsv discern/
 export function install () {
@@ -19,31 +20,31 @@ export function install () {
 
     const { home } = context.get("dir")
 
-    let _prefix = prefix
+    let _prefix = prefix || config_prefix.name
     // linux macos
     if (system === "linux" || system === "darwin") {
-        _prefix = prefix || "$NSV_HOME/prefix/bin"
+        // _prefix = join(_prefix, "bin")
         // fish
         if (shell === "fish") {
             shell_config_file_content.push("set NSV_HOME $HOME/.nsv")
-            shell_config_file_content.push(`set PATH $NSV_HOME/local/node/bin ${_prefix} $PATH`)
+            shell_config_file_content.push(`set PATH $NSV_HOME/local/node/bin $NSV_HOME/${_prefix}/bin $PATH`)
             copyFile(join(home, "nsv.fish"), join(dirname(shellConfigFileDir), "functions", "nsv.fish"))
         } else {
             // bash zsh
             shell_config_file_content.push(`export NSV_HOME=$HOME/.nsv`)
             shell_config_file_content.push(`[ -s "$NSV_HOME/nsv.sh" ] && . "$NSV_HOME/nsv.sh"`)
-            shell_config_file_content.push(`export PATH=$NSV_HOME/local/node/bin:${_prefix}:$PATH`)
+            shell_config_file_content.push(`export PATH=$NSV_HOME/local/node/bin:$NSV_HOME/${_prefix}/bin:$PATH`)
         }
 
     } else // win
     if (system === "win") {
-        _prefix = prefix || "$Env:NSV_HOME\\prefix"
+
         shell_config_file_content.push(`$Env:NSV_HOME = "${home}"`)
-        shell_config_file_content.push(`$Env:PATH = "$Env:NSV_HOME;$Env:NSV_HOME\\local\\node;${_prefix};$Env:PATH"`)
+        shell_config_file_content.push(`$Env:PATH = "$Env:NSV_HOME;$Env:NSV_HOME\\local\\node;$Env:$NSV_HOME\\${_prefix};$Env:PATH"`)
     }
 
     const npmrc_file_dir = join(process.env["HOME"], ".npmrc")
-    !prefix && writeFile(npmrc_file_dir, readFileSync(npmrc_file_dir, {encoding: "utf-8"}) + EOL + _prefix)
+    !prefix && writeFile(npmrc_file_dir, readFileSync(npmrc_file_dir, {encoding: "utf-8"}) + `${EOL}prefix=${join(home, _prefix )}`)
     shell_config_file_content.length && writeFile(shellConfigFileDir, shell_config_file_content.join(EOL), { encoding: "utf-8" })
     content && writeFile(join(context.get("dir").cache, context.get("temp_file_name")), content)
 }
@@ -53,6 +54,13 @@ export function uninstall () {
     ensureFileSync(shellConfigFileDir)
     let shell_config_file_content = readFileSync(shellConfigFileDir, { encoding: "utf-8" }).toString().split(EOL).filter(v => !test_reg.test(v))
     writeFile(shellConfigFileDir, shell_config_file_content.join(EOL), { encoding: "utf-8" })
+
+    const npmrc_file_dir = join(process.env["HOME"], ".npmrc")
+    if (existsSync(npmrc_file_dir)) {
+        const { home } = context.get("dir")
+        const prefix_reg = new RegExp(`=.*(${home}).*(prefix)`)
+        writeFile(npmrc_file_dir, readFileSync(npmrc_file_dir, {encoding: "utf-8"}).split(EOL).filter(v => !prefix_reg.test(v)).join(EOL))
+    }
 
     /**
      * 如果用的是fish  需要删除 fish functions 中的环境变量
