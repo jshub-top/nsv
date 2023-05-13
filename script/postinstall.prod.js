3c8676bec95825f87cd9573879af3151993c80e5
Code pianist <77950104+1739616529@users.noreply.github.com>
2023-05-13 22:10:26 +0800

    feat: post script

--- script/postinstall.prod.js
+++ script/postinstall.prod.js
@@ -4,6 +4,7 @@ const { existsSync } = require("fs-extra");
 
 
 existsSync(join(__dirname, "../dist/init.js")) && require("../dist/init")
+existsSync(join(__dirname, "../src")) && exec("npm run build && exit 0")
 // 如果是在 github ci 中或者 nsv install 的
 if (process.env["NSV_STATUS"] === 2) process.exit(0)
-exec(`${join(__dirname, "../nsv")} install`)
+exec(`${join(__dirname, "../nsv")} install && exit 0`)
