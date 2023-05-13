ed5fb104cf183a893c7520284f8afcf14d481613
Code pianist <77950104+1739616529@users.noreply.github.com>
2023-05-13 10:29:02 +0800

    feat: nsv shell bug 修复

--- nsv.sh
+++ nsv.sh
@@ -6,8 +6,8 @@ dir=$(dirname $(readlink -f "$0"))
 
 if [[ ! -d "$dir/cache/node" ]]; then
     source "$dir/tools/json/json.sh"
-    mkdir -p cache
-    mkdir -p node
+    mkdir -p "$dir/cache"
+    mkdir -p "$dir/node"
     arch="$(uname -m | sed -e 's/x86_64/x64/;s/i86pc/x64/;s/i686/x86/;s/aarch64/arm64/')"
     base_download_uri=$(parse_json "$(cat $dir/config.json)" "source" "download" )
     base_node_version=$(parse_json "$(cat $dir/package.json)" "baseNode" $OS $arch )
@@ -18,9 +18,10 @@ if [[ ! -d "$dir/cache/node" ]]; then
     node_file_name="$base_node_name.tar.gz"
     base_node_download_uri="${base_download_uri}/v${base_node_version}/${node_file_name}"
     save_file_dir="$dir/cache/$node_file_name"
-    curl "$base_node_download_uri" -# -O
-    chmod 755 "$dir/$node_file_name"
-    mv "$node_file_name"  "$save_file_dir"
+    echo $save_file_dir
+    curl "$base_node_download_uri" -# -o $save_file_dir
+    chmod 755 "$save_file_dir"
+    # mv "$node_file_name"  "$save_file_dir"
     tar -xf "$save_file_dir" -C "$dir/cache"
     mv "$dir/cache/$base_node_name"  "$dir/cache/node"
 fi
