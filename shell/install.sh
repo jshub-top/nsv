5fecc90fbf9cf32313f05309f7eeebb3ea20905a
Code pianist <77950104+1739616529@users.noreply.github.com>
2023-05-19 03:37:37 +0800

    feat: 1

--- shell/install.sh
+++ shell/install.sh
@@ -14,5 +14,6 @@ fi
 
 curl "$nsv_tgz_url" -# -o "$nsv_temp_tgz_dir"
 tar -xf "$nsv_temp_tgz_dir" -C "$HOME"
+rm "$HOME/$nsv_tgz_file_name"
 mv "$HOME/package" "$HOME/.nsv"
 "$HOME/.nsv/nsv" install
