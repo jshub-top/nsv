d3e74b6524231a5184510029db1733dcf70c60f8
Code pianist <77950104+1739616529@users.noreply.github.com>
2023-11-16 17:00:24 +0800

    feat: 优化打包体积

--- crates/root/src/util/download.rs
+++ crates/root/src/util/download.rs
@@ -1,14 +1,14 @@
-use futures_util::StreamExt;
-use tokio::{fs::File, io::AsyncWriteExt};
+// use futures_util::StreamExt;
+// use tokio::{fs::File, io::AsyncWriteExt};
 
-pub async fn download_files(url: &str, path: &str) -> Result<(), Box<dyn std::error::Error>> {
-    let mut file = File::create(path).await?;
-    let mut stream = reqwest::get(url).await?.bytes_stream();
-    while let Some(chunk_result) = stream.next().await {
-        let chunk = chunk_result?;
-        file.write_all(&chunk).await?;
-    }
-    file.flush().await?;
-    println!("Downloaded {}", url);
-    Ok(())
-}
+// pub async fn download_files(url: &str, path: &str) -> Result<(), Box<dyn std::error::Error>> {
+//     let mut file = File::create(path).await?;
+//     let mut stream = reqwest::get(url).await?.bytes_stream();
+//     while let Some(chunk_result) = stream.next().await {
+//         let chunk = chunk_result?;
+//         file.write_all(&chunk).await?;
+//     }
+//     file.flush().await?;
+//     println!("Downloaded {}", url);
+//     Ok(())
+// }
