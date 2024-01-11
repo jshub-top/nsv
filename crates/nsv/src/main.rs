1ff7ffe0bb4dae549141205ba6bf60142344745e
Code pianist <77950104+1739616529@users.noreply.github.com>
2024-01-11 11:30:59 +0800

    feat: 修改测试环境node镜像源

--- crates/nsv/src/main.rs
+++ crates/nsv/src/main.rs
@@ -8,9 +8,12 @@ use root::{config::Config, core::{NsvCore, init::Init}};
 async fn main() {
     let cli = parse();
     let mut config = Config::build();
-    if cfg!(debug_assertions) {
-        config.origin = "http://127.0.0.1:3000"
+
+    #[cfg(debug_assertions)]
+    {
+        config.origin = "http://127.0.0.1:3000";
     }
+
     let mut nsv_core = NsvCore::build(config);
     nsv_core.init().await;
     cli.subcmd.call(cli.config, &mut nsv_core).await
