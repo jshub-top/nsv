cb967cce7837e145e5ae37aa076620fc698a61d1
Code pianist <77950104+1739616529@users.noreply.github.com>
2024-07-15 11:11:26 +0800

    feat: cdn file

--- crates/serve/src/controller/cdn.rs
+++ crates/serve/src/controller/cdn.rs
@@ -1,35 +1,23 @@
-use std::{env, sync::Arc};
-use std::os::windows::fs::MetadataExt;
-use axum::{
-    extract::{Request},
-};
-use axum::body::{Body};
+use crate::{app::ShareState, util::file::download_file};
+use axum::body::Body;
+use axum::extract::Request;
 use axum::extract::State;
-use axum::http::{header, HeaderMap, HeaderName, HeaderValue};
-use axum::response::IntoResponse;
-use hyper::Uri;
-use sea_orm::JsonValue::String;
+use axum::http::{header, HeaderMap, HeaderValue};
+use std::{env, sync::Arc};
 use tokio::fs::{self, File};
 use tokio_util::io::ReaderStream;
-use crate::{app::ShareState, util::file::download_file};
-use crate::util::file::{stream_to_file};
 
-
-pub async fn cache_by_url(State(state): State<Arc<ShareState>>, request: Request) -> (HeaderMap, Body) {
+pub async fn cache_by_url(
+    State(state): State<Arc<ShareState>>,
+    request: Request,
+) -> (HeaderMap, Body) {
     let uri = request.uri();
     let mut current_exe_path = env::current_exe().unwrap();
     current_exe_path.pop();
     uri.path().split("/").for_each(|x| current_exe_path.push(x));
 
     println!("file not exist: {}", current_exe_path.display());
-    if fs::metadata(&current_exe_path).await.is_ok() {
-        // let file = File::open(&current_exe_path).await.unwrap();
-        // let meta = file.metadata().await.unwrap();
-        // let create_time = meta.creation_time().unwrap();
-        // if create_time.as_secs() > state.config.cache_time {
-        //     stream_to_file(&current_exe_path, &state.config.base_url, uri.path()).await.unwrap();
-        // }
-    } else {
+    if fs::metadata(&current_exe_path).await.is_err() {
         println!("file not exist: {}", current_exe_path.display());
         let url = format!("{}{}", state.config.base_url, uri.path());
         download_file(&url, &current_exe_path).await.unwrap();
@@ -44,10 +32,7 @@ pub async fn cache_by_url(State(state): State<Arc<ShareState>>, request: Request
         header::CONTENT_TYPE,
         HeaderValue::from_str("application/x-7z-compressed").unwrap(),
     );
-    header_map.insert(
-        header::CONTENT_LENGTH,
-        HeaderValue::from(file_meta.len()),
-    );
+    header_map.insert(header::CONTENT_LENGTH, HeaderValue::from(file_meta.len()));
     header_map.insert(
         header::CONTENT_DISPOSITION,
         HeaderValue::from_str(&format!("attachment; filename={}", name)).unwrap(),
--- crates/serve/src/router/mod.rs
+++ crates/serve/src/router/mod.rs
@@ -1,9 +1,10 @@
 pub mod cdn;
 use axum::Router;
 use std::sync::Arc;
-
 use crate::app::ShareState;
 
+
 pub fn create_router(share_state: Arc<ShareState>) -> Router {
-    Router::new().nest("/cdn", cdn::router(share_state))
+    Router::new()
+        .nest("/cdn", cdn::router(share_state))
 }
