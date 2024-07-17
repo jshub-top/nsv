d98b77cd70c0838bd49b2de873efed85ebdac094
Code pianist <77950104+1739616529@users.noreply.github.com>
2024-07-17 16:24:26 +0800

    feat: root config add default data

--- crates/root/src/config/mod.rs
+++ crates/root/src/config/mod.rs
@@ -1,19 +1,19 @@
-
 #[derive(Debug, Clone)]
 pub struct Config {
     pub origin: &'static str,
-
 }
 
 impl Config {
     pub fn build() -> Self {
-        let config = Config {
-            origin: "https://nodejs.org",
-        };
+        let config = Config::default();
         config
     }
+}
 
-    pub fn set_config(&mut self, call_fn: Box<dyn FnOnce(&mut Config) -> ()>)  {
-        call_fn(self);
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            origin: "https://nodejs.org/dist",
+        }
     }
 }
