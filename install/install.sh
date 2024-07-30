ff83772a52e343aed69512e2121aa63df05c3ea4
Code pianist <77950104+1739616529@users.noreply.github.com>
2024-07-30 14:29:16 +0800

    feat: install.sh

---
+++ install/install.sh
@@ -0,0 +1,98 @@
+#!/bin/sh
+
+if [[ -z "$NSV_HOME" ]]; then
+    NSV_HOME=$(pwd)
+fi
+OS=$(uname | sed 'y/ABCDEFGHIJKLMNOPQRSTUVWXYZ/abcdefghijklmnopqrstuvwxyz/')
+log_file="$NSV_HOME/install.log"
+
+NSV_PROFILE="$HOME/.nsv_profile_sh"
+
+
+set_profile_content() {
+
+    sh_profile_content="
+[[ -f ~/.nsv_profile_sh ]] && source ~/.nsv_profile_sh"
+    echo "$sh_profile_content" >> ~/.bashrc
+    echo "set sh profile" >> $log_file
+    echo "$sh_profile_content" >> $log_file
+
+    nsv_sh_profile_content="
+#!/bin/sh
+timestamp=\$(date +%s)
+export NSV_HOME=$NSV_HOME
+export PATH=\$NSV_HOME/temp/\$timestamp:\$NSV_HOME/temp/default:\$NSV_HOME:\$PATH
+    "
+    echo "$nsv_sh_profile_content" > "$NSV_PROFILE"
+    echo "set nsv profile" >> $log_file
+    echo "$nsv_sh_profile_content" >> $log_file
+
+}
+
+
+download_file() {
+    echo "download file" >> $log_file
+    echo "url: $1" >> $log_file
+    echo "output: $2" >> $log_file
+    status_code=$(curl -fsSL -# -o $2 -w "%{http_code}" "$1")
+    if [ "$status_code" -ne 200 ]; then
+
+        echo "download file error"
+        echo "status code: $status_code"
+        echo "url: $1"
+        echo "output: $2"
+
+        echo "download file error" >> $log_file
+        echo "status code: $status_code" >> $log_file
+        echo "url: $1" >> $log_file
+        echo "output: $2" >> $log_file
+
+        exit 1
+
+    fi
+}
+
+get_nsv_file_name() {
+    arch="$(uname -m | sed -e 's/x86_64/x64/;s/i86pc/x64/;s/i686/x86/;s/aarch64/arm64/')"
+    echo "nsv-$arch-$OS"
+}
+
+download_nsv_binary() {
+    nsv_download_url="https://github.com/1739616529/nsv/releases/download/v0.0.1/nsv-x64-win.exe"
+    # nsv_download_url="https://github.com/1739616529/nsv/releases/download/v0.0.1/$(get_nsv_file_name)"
+    nsv_binary_path="$NSV_HOME/nsv"
+    download_file "$nsv_download_url" "$nsv_binary_path"
+    chmod 755 $nsv_binary_path
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# set profile
+set_profile_content
+download_nsv_binary
+
+echo -e "✨✨✨
+
+    \033[34mnsv install success.\033[0m
+    \033[31mPlease reload the user environment variables...\033[0m
+
+✨✨✨
+"
+echo ""
+
+echo "nsv install soccess" >> $log_file
