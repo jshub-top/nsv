ce0c5b6ae7df0ca464a0899a067ca1afac8c0a84
Code pianist <77950104+1739616529@users.noreply.github.com>
2024-09-06 00:30:31 +0800

    workflow: build windows

--- script/ci/build.windows.sh
+++ script/ci/build.windows.sh
@@ -4,6 +4,6 @@ rustup target add x86_64-pc-windows-msvc
 
 wait
 
-cargo build --bin nsv --release --target aarch64-pc-windows-msvc
-#cargo build --bin nsv --release --target i686-pc-windows-msvc
-#cargo build --bin nsv --release --target x86_64-pc-windows-msvc
+cargo build --bin nsv --release --target aarch64-pc-windows-msvc &
+cargo build --bin nsv --release --target i686-pc-windows-msvc &
+cargo build --bin nsv --release --target x86_64-pc-windows-msvc
