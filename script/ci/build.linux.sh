0c2bf7f595c548896af187a6cc71f95b436aa9a0
Code pianist <77950104+1739616529@users.noreply.github.com>
2024-09-06 00:34:21 +0800

    workflow: build windows

--- script/ci/build.linux.sh
+++ script/ci/build.linux.sh
@@ -2,6 +2,8 @@ cargo install cross
 
 wait
 
-cross build --bin nsv --release --verbose --target aarch64-unknown-linux-gnu
-cross build --bin nsv --release --verbose --target i686-unknown-linux-gnu
+cross build --bin nsv --release --verbose --target aarch64-unknown-linux-gnu &
+cross build --bin nsv --release --verbose --target i686-unknown-linux-gnu &
 cargo build --bin nsv --release --verbose --target x86_64-unknown-linux-gnu
+
+wait
--- script/ci/build.windows.sh
+++ script/ci/build.windows.sh
@@ -7,3 +7,5 @@ wait
 cargo build --bin nsv --release --target aarch64-pc-windows-msvc &
 cargo build --bin nsv --release --target i686-pc-windows-msvc &
 cargo build --bin nsv --release --target x86_64-pc-windows-msvc
+
+wait
