190443ad5f0ccd299aa18ec5efb670cec60cfe9b
Code pianist <77950104+1739616529@users.noreply.github.com>
2024-09-06 00:50:09 +0800

    workflow: build windows for cross change ubuntu

--- script/ci/build.windows.sh
+++ script/ci/build.windows.sh
@@ -1,11 +1,20 @@
-rustup target add aarch64-pc-windows-msvc &
-rustup target add i686-pc-windows-msvc &
-rustup target add x86_64-pc-windows-msvc
+#rustup target add aarch64-pc-windows-msvc &
+#rustup target add i686-pc-windows-msvc &
+#rustup target add x86_64-pc-windows-msvc
+#
+#wait
+#
+#cargo build --bin nsv --release --target aarch64-pc-windows-msvc &
+#cargo build --bin nsv --release --target i686-pc-windows-msvc &
+#cargo build --bin nsv --release --target x86_64-pc-windows-msvc
+#
+#wait
 
-wait
 
-cargo build --bin nsv --release --target aarch64-pc-windows-msvc &
-cargo build --bin nsv --release --target i686-pc-windows-msvc &
-cargo build --bin nsv --release --target x86_64-pc-windows-msvc
+cargo install cross
+
+
+cross build --bin nsv --release --target i686-pc-windows-gnu &
+cross build --bin nsv --release --target x86_64-pc-windows-gnu
 
 wait
