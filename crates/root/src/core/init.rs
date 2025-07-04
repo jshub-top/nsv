289053203522f11f2f7e59b274a6f5833461139f
Code pianist <77950104+1739616529@users.noreply.github.com>
2025-07-04 20:27:04 +0800

    feat: context add local node version list

--- crates/root/src/core/init.rs
+++ crates/root/src/core/init.rs
@@ -30,18 +30,30 @@ impl Init for NsvCore {
             Box::pin(ensure_dir(&node_file)),
             Box::pin(ensure_dir(&temp_dir)),
         ];
-        let _ = futures::future::join_all(dir_futures).await;
+        futures::future::join_all(dir_futures).await;
 
         // 初始化之后运行
         let dir_futures: Vec<Pin<Box<dyn Future<Output = Result<()>> + Send>>> = vec![
             Box::pin(self.set_local_version()),
         ];
-        futures::future::join_all(dir_futures).await?;
+        futures::future::join_all(dir_futures).await;
 
+        Ok(())
     }
 
-    async fn set_local_version(&mut self) -> io::Result<()> {
-        let local_version = read_dir(self.context.node_dir.as_path()).await?;
+    async fn set_local_version(&mut self) -> Result<()> {
+        let mut local_versions = Vec::new();
+        let mut entries = read_dir(self.context.node_dir.as_path()).await?;
+
+        while let Some(entry) = entries.next_entry().await? {
+            if entry.file_type().await?.is_dir() {
+                if let Some(name) = entry.file_name().to_str() {
+                    local_versions.push(name.to_string());
+                }
+            }
+        }
+
+        self.context.local_version = local_versions;
         Ok(())
     }
 }
